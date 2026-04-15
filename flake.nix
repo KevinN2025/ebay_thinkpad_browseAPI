06d4654a61ea3d46bc0015340fb0667ae50ca51f
kevin noguera <kevin5631noguera@gmail.com>
2026-04-15 16:45:40 -0400

    Added: flake.nix

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "ebay_find development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            lazysql
+            go
+            gnumake
+          ];
+
+          shellHook = ''
+            cat > "$HOME/.config/lazysql/config.toml" << 'EOF'
+[[database]]
+Name = 'Local MariaDB'
+Provider = 'mysql'
+DBName = 'ebay_find'
+URL = 'mysql://KevinN:Eagles7713%40127.0.0.1:3306/ebay_find'
+
+[application]
+DefaultPageSize = 400
+DisableSidebar = false
+SidebarOverlay = false
+JSONViewerWordWrap = false
+EnterOpensJSONViewer = false
+EOF
+            echo "lazysql config written to ~/.config/lazysql/config.toml"
+          '';
+        };
+      }
+    );
+}
